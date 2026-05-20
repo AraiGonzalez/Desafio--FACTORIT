709bb2da17f432d893fbdda5fc667a348d8fe973
AraiGonzalez <araiestefania94@hotmail.com>
2026-05-19 21:26:12 -0300

    Remove front folder

--- ecommerce-frontend/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { CartComponent } from './components/cart/cart.component';
-import { CustomersComponent } from './components/customers/customers.component';
-export const routes: Routes = [
-  { path: '', redirectTo: 'cart', pathMatch: 'full' },
-  { path: 'cart', component: CartComponent },
-  { path: 'customers', component: CustomersComponent },
-];
